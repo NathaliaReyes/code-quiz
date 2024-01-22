cb3ba746591393b168bcbe16fdd91d340c0ad6b6
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-01-21 17:26:04 -0700

    Funcionality go-back and clean-scores bottons added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,11 @@
-var timeEl = document.querySelector(".time");
+var timeEl = document.querySelector(".timer");
 var mainEl = document.getElementById("main");
-var startBtn = document.getElementById("btn-start");
+var startBtn = document.querySelector(".btn-start")
 var endEl = document.getElementById("end");
 var quizEl = document.getElementById("quiz");
+var btnSubmit = document.querySelector(".btn-submit")
+var userInitials = document.getElementById("initials");
+var formEl = document.getElementById("initials-form");
 var score = 0;
 var questionIndex = 0;
 var secondsLeft = 90;
@@ -31,7 +34,7 @@ var questions = [
     },
     {
         question: 'How is a for loop written in JavaScript?',
-        answers: ['for (i = 0; i < 5; i++)', 'for (i = 0; i < 5)', 'for (i = 0; i < 5; i--)', 'for (i = 0; i < 5; i++)'],
+        answers: ['for (i = 0; i < 5; i++)', 'for (i = 0; i < 5)', 'for (i = 0; i < 5; i--)', 'for (i = 0, i < 5, i++)'],
         correctAnswer: 'for (i = 0; i < 5; i++)'
     },
     {
@@ -56,9 +59,6 @@ var questions = [
     }
 ];
 
-
-
-
 function startGame() {
     mainEl.style.display = "none";
     secondsLeft = 90;
@@ -68,18 +68,14 @@ function startGame() {
 
 startBtn.addEventListener("click", startGame);
 
-
-
 function setTime() {
     // Sets interval in variable
     timerInterval = setInterval(function () {
         secondsLeft--;
         document.querySelector(".timer").textContent = secondsLeft;
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             clearInterval(timerInterval);
-            document.getElementById("end").style.display = "block"; // show the end screen
-            document.getElementById("final-score").textContent = score;// show the final score
-            mainEl.style.display = "none";
+            endGame();
         }
     }, 1000);
 }
@@ -93,6 +89,7 @@ function showQuestions() {
         var button = document.createElement("button");
         button.style.display = "block";
         button.textContent = answer;
+        button.className = "btn-answer";
         button.addEventListener("click", checkAnswer);
         answers.appendChild(button);
     });
@@ -104,6 +101,7 @@ function checkAnswer() {
         score++;
         console.log()
     } else {
+        document.getElementById("result").innerHTML = "Wrong!";
         secondsLeft -= 10;
     }
     questionIndex++;
@@ -115,16 +113,74 @@ function checkAnswer() {
     }
 }
 
-
-
-
 // Function that ends the game
 function endGame() {
     clearInterval(timerInterval);
+    timeEl.innerHTML = '';
     document.getElementById("end").style.display = "block"; // show the end screen
     document.getElementById("final-score").textContent = score;// show the final score
     quizEl.style.display = "none"; 
 }
+
+function saveLastScore() {
+    //Save related form data as an object
+    var userScore = {
+        user: userInitials.value,
+        score : score
+    };
+
+    // Get the existing scores
+    var scores = JSON.parse(localStorage.getItem('scores')) || [];
+    // Add the new score
+    scores.push(userScore);
+    // Save the scores
+    localStorage.setItem('scores', JSON.stringify(scores));
+};
+
+function renderLastScore() {
+    // Get the scores
+    var scores = JSON.parse(localStorage.getItem('scores')) || [];
+    // Get the list
+    var list = document.getElementById('showScores');
+    // Clear the list
+    list.innerHTML = '';
+    // Add each score to the list
+    for (var i = 0; i < scores.length; i++) {
+        var li = document.createElement('li'); // create a list item
+        li.textContent = scores[i].user + ': ' + scores[i].score; // set the text
+        li.className = 'score-item';
+        list.appendChild(li); // add the list item to the list
+    }
+    var btnBack = document.createElement('button');
+    btnBack.textContent = "Go back";
+    btnBack.className = "btn-back";
+    document.querySelector(".final").appendChild(btnBack);
+
+    var btnClean = document.createElement('button');
+    btnClean.textContent = "Clear all scores";
+    btnClean.className = "btn-clean";
+    document.querySelector(".final").appendChild(btnClean);
+
+    var cleanButton = document.querySelector(".btn-clean");
+
+    cleanButton.addEventListener('click', function(){
+        localStorage.clear();
+        list.innerHTML = '';
+    });
+
+    var backButton = document.querySelector(".btn-back");
+
+    backButton.addEventListener('click', function(){
+        location.reload();
+    });
+}
+
+btnSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+    formEl.style.display = "none";
+    saveLastScore();
+    renderLastScore();
+})
 
 
 
